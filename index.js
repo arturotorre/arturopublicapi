09c3e1e53e70f395dfa30c896a0d639c82d7282a
Arturo Torres <arturo.torre@outlook.com>
2023-11-14 17:12:40 -0600

    Categories and Languages working at 100%

--- index.js
+++ index.js
@@ -12,28 +12,44 @@ const port = 3000;
 app.use(express.static("public"));
 
 //We activate bodyparser to parse data from user's request
+app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//We set de engine as ejs so jQuery can work in this type of file.
-app.set("view engine", "ejs");
+// //We set de engine as ejs so jQuery can work in this type of file.
+// app.set("view engine", "ejs");
 
 // When the user goes to the home page it should render the index.ejs file.
 app.get("/", async (req, res) => {
     try {
-      const result = await axios.get("https://v2.jokeapi.dev/joke/any");
+      const result = await axios.get("https://v2.jokeapi.dev/joke");
       res.render("index.ejs", {
         joke: result.data.joke,
         setup : result.data.setup,
         delivery: result.data.delivery
         });
     } catch (error) {
-      res.render("index.ejs", { firstJoke: JSON.stringify(error.data) });
+      res.render("index.ejs", { joke: JSON.stringify(error.data) });
     }
 });
 
+app.post("/get-joke", async (req, res) => {
+    const jokeCategory = req.body.category;
+    const jokeLanguage = req.body.language;
+    const jokeQty = req.body.qty;
+    try {
+        const result = await axios.get("https://v2.jokeapi.dev/joke/"+jokeCategory,{params: { lang: jokeLanguage} });
+        res.render("index.ejs", {
+            joke: result.data.joke,
+            setup : result.data.setup,
+            delivery: result.data.delivery
+        });
+    } catch (error) {
+    res.render("index.ejs", { joke: JSON.stringify(error.data) });
+    }
+  });
+
 
 // Listen on your predefined port and start the server.
-
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
   });
